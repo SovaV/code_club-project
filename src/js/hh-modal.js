b117bb31a261666e11cfa2b709e9728a62aca4f7
SovaV <sova.volodymyr95@gmail.com>
2021-04-28 16:50:41 +0300

    скріпт нарешт

--- src/js/hh-modal.js
+++ src/js/hh-modal.js
@@ -1,12 +1,10 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open-mb]'),
-    closeModalBtn: document.querySelector('[data-modal-close-mb]'),
-    modal: document.querySelector('[data-modal-viw]'),
+    modal: document.querySelector('[data-modal-mb]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
